2f406bea1021e970ea72fef7ec73767fa50b5265
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-20 21:47:17 -0300

    finalizando o search estatico

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -7,7 +7,7 @@ export const ContentPokedex = () => {
                 <h1 className={styles.choose}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
             </article>
             <article>
-                <input type="text"/>
+                <input type="text" placeholder='Encuentra tu pokémon...' className={styles.search}/>
             </article>
         </article>
     )
