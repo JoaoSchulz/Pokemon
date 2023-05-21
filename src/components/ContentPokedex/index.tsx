ebe26aa97860f5c7ff08058e712bf636b81609fb
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-20 21:42:11 -0300

    começando search e frase topo

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -1,7 +1,14 @@
 import styles from './styles.module.css'
 
 export const ContentPokedex = () => {
-    return(
-        <h1></h1>
+    return (
+        <article className={styles.conteiner}>
+            <article>
+                <h1 className={styles.choose}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
+            </article>
+            <article>
+                <input type="text"/>
+            </article>
+        </article>
     )
 }
