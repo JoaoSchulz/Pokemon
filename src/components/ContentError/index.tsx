16bb6065d80a9d54946eea8a49412eaaeb318fb1
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-22 20:43:04 -0300

    finalizando o estatico da tela

--- src/components/ContentError/index.tsx
+++ src/components/ContentError/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.css'
 import equipeRocket from '../../assets/Error/Team_Rocket.svg'
+import { NavLink } from 'react-router-dom'
 
 export const ContentError = () => {
     return (
@@ -11,9 +12,9 @@ export const ContentError = () => {
                 <img src={equipeRocket} alt="Foto central da equipe rocket" className={styles.img_rocket} />
                 <article className={styles.conteiner_texts}>
                     <h1 className={styles.conteiner_texts_white}>The rocket team</h1>
-                    <h1>has won this time.</h1>
+                    <h1 className={styles.conteiner_texts_black}>has won this time.</h1>
                 </article>
-                <button>Return</button>
+                <button className={styles.conteiner_button}> <NavLink to={`/`} className={styles.conteiner_button_link}>Return</NavLink></button>
             </article>
         </section>
     )
