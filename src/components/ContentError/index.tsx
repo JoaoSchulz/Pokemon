f98f0a72bfdd34d4b586ac06b310afedf15ba743
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-22 19:46:35 -0300

    colocando os componentes

--- src/components/ContentError/index.tsx
+++ src/components/ContentError/index.tsx
@@ -1,9 +1,17 @@
 import styles from './styles.module.css'
+import equipeRocket from '../../assets/Error/Team_Rocket.svg'
 
 export const ContentError = () => {
     return (
         <section>
-            
+            <article>
+                <h1>404</h1>
+            </article>
+            <article>
+                <img src={equipeRocket} alt="Foto central da equipe rocket" />
+                <h1></h1>
+                <button></button>
+            </article>
         </section>
     )
 }
