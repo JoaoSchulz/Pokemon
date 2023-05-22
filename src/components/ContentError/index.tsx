18d48eff3169a604d9839be89ad7c7c60fd759cd
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-22 20:18:03 -0300

    ajeitando tela

--- src/components/ContentError/index.tsx
+++ src/components/ContentError/index.tsx
@@ -3,14 +3,17 @@ import equipeRocket from '../../assets/Error/Team_Rocket.svg'
 
 export const ContentError = () => {
     return (
-        <section>
-            <article>
+        <section className={styles.conteiner}>
+            <article className={styles.conteiner_text_erro}>
                 <h1>404</h1>
             </article>
-            <article>
-                <img src={equipeRocket} alt="Foto central da equipe rocket" />
-                <h1></h1>
-                <button></button>
+            <article className={styles.conteiner_front}>
+                <img src={equipeRocket} alt="Foto central da equipe rocket" className={styles.img_rocket} />
+                <article className={styles.conteiner_texts}>
+                    <h1 className={styles.conteiner_texts_white}>The rocket team</h1>
+                    <h1>has won this time.</h1>
+                </article>
+                <button>Return</button>
             </article>
         </section>
     )
---
+++ src/routes/Error/Erro.tsx
@@ -0,0 +1,9 @@
+import { ContentError } from "../../components/ContentError"
+
+export const Erro = () => {
+    return (
+        <>
+            <ContentError/>
+        </>
+    )
+}
