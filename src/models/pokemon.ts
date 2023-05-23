76edb1dc2f7f778abfaf4c78621fcf26a9934449
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-23 14:30:38 -0300

    model pokemon

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,1 +1,21 @@
-export type pokemon 
+type PokemonType = {
+    type: string
+}
+
+export type Request = {
+    id: number;
+    types: PokemonType[]
+    image: string
+    attack: number
+    defense: number
+}
+export type Pokemon = {
+    name: string
+    url: string
+    id: number
+    types: PokemonType[]
+    image: string
+    attack: number
+    defense: number
+}
+"
